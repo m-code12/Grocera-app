0adc236c5e3009ce62dca6b7c391324a5242d2e2
cheshtha agarwal <cheshthaagarwal@yahoo.in>
2020-08-24 18:04:37 +0530

    delete

--- dist/buyers.js
+++ dist/buyers.js
@@ -12,7 +12,7 @@ rootRef.on("child_added", snap => {
 	var code = snap.child("pinCode").val();
 	
 	//$("#table_body").append("<tr><td contenteditable='true'>" + id + "</td><td contenteditable='true'>" + name + "</td><td contenteditable='true'>" + number + "</td><td contenteditable='true'>" + code + "</td><td contenteditable='true'>" + token + "</td><td><button onclick='deleteRow(this)'>Remove</button></td></tr>")
-	$('#table_body').append('<tr><td contenteditable="true">' + id + '</td><td contenteditable="true">' + name + '</td><td contenteditable="true">' + number + '</td><td contenteditable="true">' + code + '</td><td contenteditable="true">' + token + '</td><td><button class="btn btn-outline-danger" onclick="deleteRow(this)"><b>Remove</b></button></td></tr>')
+	$('#table_body').append('<tr><td contenteditable="true">' + id + '</td><td contenteditable="true">' + name + '</td><td contenteditable="true">' + number + '</td><td contenteditable="true">' + code + '</td><td contenteditable="true">' + token + '</td><td><button onclick=deletebuyer("' + snap.key + '") class="btn btn-outline-danger" ><b>Remove</b></button></td></tr>')
 });
 
 /*$(document).ready(function(){
@@ -23,6 +23,30 @@ rootRef.on("child_added", snap => {
 
 });*/
 
+function deletebuyer(key) {
+var deleteRef = firebase.database().ref().child("users").child(key);
+firebase.database().ref('users/' + key).on('value', snapshot => {
+	name = snapshot.val().name;
+	id = snapshot.val().id;
+token = snapshot.val().token;
+	data = {
+		name,
+	id,
+		token
+	};
+	
+});
+
+return deleteRef.remove()
+	.then(function() {
+		window.alert("Deleted successfully\n Refresh The Page to see the changes");
+		window.location.reload();
+	})
+	.catch(function() {
+		console.log("error occured");
+	});
+
+}
 function deleteRow(r) {
     var i = r.parentNode.parentNode.rowIndex;
     document.getElementById("dataTable").deleteRow(i);
