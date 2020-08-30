c405adda7f347a0cf151e0f42e534f2f6f1cff2d
cheshtha agarwal <cheshthaagarwal@yahoo.in>
2020-08-30 18:11:51 +0530

    products

--- dist/index.js
+++ dist/index.js
@@ -22,16 +22,31 @@ rootRef.on("child_added", snap => {
 	$('#table_body').append('<tr><td contenteditable="true">' + name + '</td><td contenteditable="true">' + email + '</td><td contenteditable="true">' + number + '</td><td contenteditable="true">' + address + '</td><td contenteditable="true">' + code + '</td><td contenteditable="true">' + fee + '</td><td contenteditable="true">' + dis + '</td><td contenteditable="true">' + rating + '</td><td contenteditable="true">' + status + '</td><td><button class="btn btn-outline-danger" onclick=products("' + snap.key + '")><b>products</b></button></td></tr>')
 	
 });
-
+function close(){
+	$('#dataTable tbody').empty();
+}
+$(".modal").on("hidden.bs.modal", function(){
+    $('#dataTable1 tbody').empty();
+});
 function products(key){
 	var rootRef = firebase.database().ref().child("productDetails");
 
 	rootRef.on("child_added", snap => {
 		var shopId = snap.child("shopId").val();
 		var name = snap.child("name").val();
+	
+		var Stock = snap.child("Stock").val();
+		var category = snap.child("category").val();
+		var stock = snap.child("stock").val();
+		var dis = snap.child("discount").val();
+		var id = snap.child("id").val();
+		var price = snap.child("price").val();
 		
+		$('#myModal').modal('show');
 	if(shopId==key){
-console.log(name);
+	
+    
+		$('#table_body2').append('<tr><td contenteditable="true">' + name + '</td><td contenteditable="true">' + shopId + '</td><td contenteditable="true">' + Stock + '</td><td contenteditable="true">' + dis + '</td><td contenteditable="true">' + category + '</td><td contenteditable="true">' + stock + '</td><td contenteditable="true">' + price + '</td></tr>')
 	}
 
 	});
